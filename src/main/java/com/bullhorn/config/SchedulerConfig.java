a5258c78157946a9c470d5432229243de6e05587
Sachin Jain <sachin.jain@bullhorn.com>
2018-06-19 15:01:41 -0400

    Changed the thread name

--- src/main/java/com/bullhorn/config/SchedulerConfig.java
+++ src/main/java/com/bullhorn/config/SchedulerConfig.java
@@ -14,7 +14,7 @@ public class SchedulerConfig implements SchedulingConfigurer {
         ThreadPoolTaskScheduler threadPoolTaskScheduler = new ThreadPoolTaskScheduler();
 
         threadPoolTaskScheduler.setPoolSize(POOL_SIZE);
-        threadPoolTaskScheduler.setThreadNamePrefix("DATA-SWAPPER-");
+        threadPoolTaskScheduler.setThreadNamePrefix("DATA-VALIDATOR-");
         threadPoolTaskScheduler.initialize();
 
         scheduledTaskRegistrar.setTaskScheduler(threadPoolTaskScheduler);
